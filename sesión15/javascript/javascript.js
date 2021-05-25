384daf54609b4cfb875c8fc4c96328bb45dcc201
Cristianrojasg2002@gmail.com <Cristianrojasg2002@hotmail.com>
2021-05-25 08:39:06 -0500

    sesion15.5

---
+++ sesión15/javascript/javascript.js
@@ -0,0 +1,36 @@
+function compara() {
+    var a = 2;
+        var b = 2;
+        if (a==b)
+        alert("a y b son iguales");
+        else
+        alert("a y b son diferentes")
+}
+
+function resta(x,y){
+    var rest = x - y;
+document.write("<h2>La resta es "+rest+"</h2");
+}
+
+function suma(a,b){
+    var sum = a + b;
+document.getElementById("sumar").innerHTML = "la suma es " + sum;
+}
+
+function escribir(){
+    valor = document.getElementById('entrada').value;
+    document.getElementById('contenido').innerHTML=' '+valor;
+  
+  }
+  
+
+//   function escribir(){
+//     valor = document.getElementById('entrada').value;
+//     document.getElementById('contenido').innerHTML=' '+valor;
+  
+//   }
+
+var caja = document.getElementsByTagName('div');
+
+caja[0].innerHTML = '<h1>Hola</h1>';
+  
