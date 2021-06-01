f9e50f80c39ff780f59a64a30b6845e6d2d5e6ed
Cristianrojasg2002@gmail.com <Cristianrojasg2002@hotmail.com>
2021-06-01 01:04:40 -0500

    sesion 17

---
+++ sesion17/JAVASCRIPT/jscript3.js
@@ -0,0 +1,55 @@
+function guardarDatos(){
+    const nombre = document.getElementById("nombre").value;
+    const movil = document.getElementById("movil").value;
+    const email = document.getElementById("email").value;
+
+    const datos = {
+        'movil':movil,
+        'email':email,
+    };
+
+
+    localStorage.setItem(nombre, JSON.stringify(datos))
+    document.getElementById("nombre").value="";
+    document.getElementById("movil").value="";
+    document.getElementById("email").value="";
+    actualizarDatos();
+}
+
+function recuperarDato(){
+    const nombre = document.getElementById("nombre").value;
+    const nombre1 = localStorage.getItem(nombre);
+    const datosCom = JSON.parse(nombre1);
+    document.getElementById("movil").value = datosCom.movil;
+    document.getElementById("email").value = datosCom.email;
+
+}
+
+function eliminarDato(){
+    const nombre=document.getElementById("nombre").value;
+    localStorage.removeItem(nombre);
+    actualizarDatos();
+}
+
+function eliminarTodo(){
+    localStorage.clear();
+    actualizarDatos();
+}
+
+function actualizarDatos(){
+    var registro = "";
+    if(localStorage.legth===0){
+        registro +='<li>Vacío<li>';
+
+    } else {
+        for(var i=0;i<=localStorage.length-1;i++){
+            const key = localStorage.key(i);
+            const datosR = localStorage.getItem(key);
+            const datosCom = JSON.parse(datosR);
+            registro += '<li>' + '<span class="nom">' + key + '</span>'
+            + '<span class="nom">' + datosCom.movil + '</span>' + '<span class="nom">' + datosCom.email + '</span>'+ '</li><br>';
+        }
+
+    }
+    document.getElementById('contactos').innerHTML=registro;
+}
